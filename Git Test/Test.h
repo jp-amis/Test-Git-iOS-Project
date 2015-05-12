e3df897e821905feb35d291bfa44c7668258683e
João Paulo Pinheiro Teixeira <juaumlol@gmail.com>
2015-05-12 00:45:13 -0300

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by João Paulo Pinheiro Teixeira on 5/12/15.
+//  Copyright (c) 2015 Amis.co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by João Paulo Pinheiro Teixeira on 5/12/15.
+//  Copyright (c) 2015 Amis.co. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
